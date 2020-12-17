f56bb4aa6ca80572530b90873f049986a9139822
BOUYACOUB Kamel <kamel_bouyacoub07@live.fr>
2020-12-17 23:12:05 +0100

    Create EventBusRabbitMq project

---
+++ EventBusRabbitMQ/Common/EventBusConstant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ.Common
+{
+    public static class EventBusConstant
+    {
+        public const string BasketChekoutQueue = "basketCheckoutQueue";
+    }
+}
