fe82a976ae67332e8106bd041f76a5190e7b8c39
BOUYACOUB Kamel <kamel_bouyacoub07@live.fr>
2020-12-20 18:13:17 +0100

    Create entity base classes

---
+++ Ordering.Core/Entities/Base/EnityBase.cs
@@ -0,0 +1,38 @@
+﻿namespace Ordering.Core.Entities.Base
+{
+    public class EnityBase<T> : IEntityBase<T>
+    {
+        public virtual T Id { get; protected set; }
+        int? _requestedHashCode;
+
+        public bool IsTransient()
+        {
+            return Id.Equals(default(T));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is EnityBase<T>)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (GetType() != obj.GetType()) return false;
+            var item = (EnityBase<T>)obj;
+            if (item.IsTransient() || IsTransient()) 
+                return false;
+            else 
+                return item == this;
+        }
+
+        public static bool operator ==(EnityBase<T> left, EnityBase<T> right)
+        {
+            if (Equals(left, null))
+                return Equals(right, null) ? true : false;
+            else
+                return left.Equals(right);
+        }
+
+        public static bool operator !=(EnityBase<T> left, EnityBase<T> right)
+        {
+            return !(left == right);
+        }
+    }
+}
--- Ordering.Core/Entities/Order.cs
+++ Ordering.Core/Entities/Order.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Ordering.Core.Entities
 {
-    public class Order
+    public class Order : Entity
     {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAdress { get; set; }
+        public string AddressLine { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public int PaymentMethod { get; set; }
     }
 }
