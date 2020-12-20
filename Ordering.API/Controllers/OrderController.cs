2d58ad13c3471e97b7ae40bf9b883ed71dce54e4
BOUYACOUB Kamel <kamel_bouyacoub07@live.fr>
2020-12-20 01:19:06 +0100

    Create an web rest api Order.api

---
+++ Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+  
+ 
+ 
+    }
+}
