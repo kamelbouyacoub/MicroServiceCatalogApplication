162c95be99a48738739ec7d8843d0f03dfb44824
BOUYACOUB Kamel <kamel_bouyacoub07@live.fr>
2020-12-26 01:25:13 +0100

    Implement GetOrderByUserName and CheckOutOrder method's  controller

--- Ordering.API/Controllers/OrderController.cs
+++ Ordering.API/Controllers/OrderController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Orderning.Application.Commands;
+using Orderning.Application.Queries;
+using Orderning.Application.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Ordering.API.Controllers
@@ -11,8 +16,29 @@ namespace Ordering.API.Controllers
     [Route("[controller]")]
     public class OrderController : ControllerBase
     {
-  
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
  
- 
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<OrderResponse>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<IEnumerable<OrderResponse>>> GetOrderByUserName(string userName)
+        {
+            var query = new GetOrderByUserNameQuery(userName);
+            var orders = await _mediator.Send(query);
+            return Ok(orders);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(OrderResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrderResponse>>> CheckOutOrder([FromBody]CheckoutOrderCommand command)
+        {
+   
+            var result  = await _mediator.Send(command);
+            return Ok(result);
+        }
     }
 }
