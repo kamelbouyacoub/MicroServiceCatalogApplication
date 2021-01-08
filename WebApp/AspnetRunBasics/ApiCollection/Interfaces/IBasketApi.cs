6f142388baa4cc58747bd61c69033341d89107c5
BOUYACOUB Kamel <kamel_bouyacoub07@live.fr>
2021-01-08 02:32:14 +0100

    Create web front application

---
+++ WebApp/AspnetRunBasics/ApiCollection/Interfaces/IBasketApi.cs
@@ -0,0 +1,15 @@
+﻿using AspnetRunBasics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection.Interfaces
+{
+    public interface IBasketApi
+    {
+        Task<IEnumerable<BasketModel>> GetBasket(string username);
+        Task<IEnumerable<BasketModel>> UpdateBasket(BasketModel model);
+        Task CheckoutBasket(BasketCheckoutModel model);
+    }
+}
---
+++ WebApp/AspnetRunBasics/ApiCollection/Interfaces/ICatalogApi.cs
@@ -0,0 +1,16 @@
+﻿using AspnetRunBasics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection.Interfaces
+{
+    public interface ICatalogApi
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+        Task<IEnumerable<CatalogModel>> GetCatalogById(string category);
+        Task<IEnumerable<CatalogModel>> CreateCatalogBy(string category);
+    }
+}
